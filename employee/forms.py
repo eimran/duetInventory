bd0ea63addd014b4f4e290cba10503bc69f9114d
eimran <eimran.cse@gmail.com>
2020-07-23 18:17:07 +0600

    Employee Add bug slve and  Delete Done

--- employee/forms.py
+++ employee/forms.py
@@ -2,6 +2,26 @@ from django import forms
 from .models import Employee
 
 
+class EmployeeCreateForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ('first_name', 'last_name', 'cell_no', 'email', 'address', 'gender', 'dob', 'employee_no', 'joining_date', 'category')
+
+    #     widgets = {
+    #         'p_name': forms.TextInput(attrs={'class': 'form-control'}),
+    #         'country_of_origin': forms.TextInput(attrs={'class': 'form-control'}),
+    #         'brand': forms.TextInput(attrs={'class': 'form-control'}),
+    #         'p_details': forms.TextInput(attrs={'class': 'form-control'}),
+    #     }
+    #
+    # def __init__(self, *args, **kwargs):
+    #     super(EmployeeCreateForm, self).__init__(*args, **kwargs)
+    #     self.fields['p_name'].label = "Product Name"
+    #     self.fields['country_of_origin'].label = "Country of Origin"
+    #     self.fields['brand'].label = "Brand"
+    #     self.fields['p_details'].label = "Details"
+
+
 class EmployeeUpdateForm(forms.ModelForm):
     class Meta:
         model = Employee
--- employee/urls.py
+++ employee/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("profile", views.index, name="profile"),
+    path('employee-add/', views.EmployeeCreateView.as_view(), name='employee_add'),
+    path('employee/delete/<int:pk>', views.EmployeeDeleteView.as_view(), name='employee_delete'),
     path("employee-list", views.employee_list, name="employee_list"),
     path('employee/edit/<int:pk>', views.EmployeeUpdateView.as_view(), name='update_employee'),
     # path('register', views.register, name='EmployeeRegister')
